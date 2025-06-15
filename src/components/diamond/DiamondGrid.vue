7416585ab3223c7acd1278c4cc3144052de47b53
JensKrijgsman <jens.krijgsman@howest.be>
2025-06-15 13:27:19 +0200

    (refactor): Update z-index values for exercise pins in DiamondGrid component

--- src/components/diamond/DiamondGrid.vue
+++ src/components/diamond/DiamondGrid.vue
@@ -122,7 +122,7 @@
       </div>
 
       <!-- Exercise Pins -->
-      <div class="absolute inset-0" style="z-index: 30;">
+      <div class="absolute inset-0">
         <template v-for="(pin, i) in pins" :key="i">
           <div
             v-if="pin.isAddedToDiamond"
@@ -133,7 +133,8 @@
             :style="{
               left: pin.config.x + 'px',
               top: pin.config.y + 'px',
-              transform: 'translate(-50%, -50%)'
+              transform: 'translate(-50%, -50%)',
+              zIndex: 30
             }"
           >
             <!-- Pin Circle/Diamond -->
@@ -205,7 +206,7 @@
       </div>
 
       <!-- Add invisible column hover areas -->
-      <div class="absolute inset-0 pointer-events-none" style="z-index: 20;">
+      <div class="absolute inset-0 pointer-events-none" style="z-index: 25;">
         <div 
           v-for="(section, index) in sectionLabels" 
           :key="'section-' + index"
